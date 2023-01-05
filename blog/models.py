70e1aed7279cd876a3c51e296fad8d4d0317ba74
mscen <1065802928@qq.com>
2023-01-05 22:05:03 +0800

    set theme

--- blog/models.py
+++ blog/models.py
@@ -1,22 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=10)
-
-
+    name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
 class Category(models.Model):
-    name = models.CharField(max_length=10)
-
+    name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
     body = models.TextField()
-    create_time = models.DateTimeField(auto_created=True)
+    created_time = models.DateTimeField(default=timezone.now)
     modified_time = models.DateTimeField()
-    excerpt = models.CharField(max_length=200,blank=True)
-    category = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    excerpt = models.CharField(verbose_name="摘要",max_length=200,blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ForeignKey(Tag,blank=True,null=True,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def save(self,*args,**kwargs):
+        self.modified_time = timezone.now()
+        if self.excerpt == '':
+            self.excerpt = self.body[:54]
+
+        super().save(*args,**kwargs)
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .models import *
+import markdown
+from markdown.extensions.toc import TocExtension
+from django.utils.text import slugify
+from django.http import JsonResponse
 
 # Create your views here.
+
+def post(request,pk):
+    try:
+        data = Post.objects.get(id=pk)
+    except Post.DoesNotExist:
+        # TODO: msg box
+        return JsonResponse({'success': False, 'msg': "can not find it"})
+
+    md = markdown.Markdown(
+        extensions=[
+            "markdown.extensions.extra",
+            "markdown.extensions.codehilite",
+            # 记得在顶部引入 TocExtension 和 slugify
+            TocExtension(slugify=slugify),
+        ]
+    )
+    data.body = md.convert(data.body)
+    f = open('text.md',encoding='utf-8',mode='w')
+    f.write(data.body)
+    return render(request,"blog/post.html",{'data':data})
+
+
+def index(request):
+    return render(request,'blog/index.html')
